686666f80ffc734c8d7f8878f0891b3edeaec3eb
Johann <124318122+Ygn-dev@users.noreply.github.com>
2026-05-17 17:24:10 -0500

    avance en el lobby

--- Assets/Scripts/Character/Player_Controller.cs
+++ Assets/Scripts/Character/Player_Controller.cs
@@ -10,9 +10,17 @@ public class Player_Controller : MonoBehaviour
     void Update()
     {
         moveInputVector = Vector4Direcciones();
+        SetAnimations();
+        Turn();
     }
 
-    public Vector2 Vector4Direcciones()
+    void FixedUpdate()
+    {
+        //Movimiento
+        GetComponent<Rigidbody2D>().linearVelocity = new Vector2(moveInputVector.x * speed, moveInputVector.y * speed);
+    }
+
+    private Vector2 Vector4Direcciones()
     {
         Vector2 direccion = move.action.ReadValue<Vector2>();
         if (Mathf.Abs(direccion.x) > Mathf.Abs(direccion.y))
@@ -22,9 +30,28 @@ public class Player_Controller : MonoBehaviour
         return direccion;
     }
 
-    void FixedUpdate()
+    private void SetAnimations()
     {
-        //Movimiento
-        GetComponent<Rigidbody2D>().linearVelocity = new Vector2(moveInputVector.x * speed, moveInputVector.y * speed);
+        if (moveInputVector != Vector2.zero) GetComponent<Animator>().SetBool("isRunning", true);
+        else GetComponent<Animator>().SetBool("isRunning", false);
+
+        if (moveInputVector == Vector2.zero) return;
+        GetComponent<Animator>().SetFloat("moveX", moveInputVector.x);
+        GetComponent<Animator>().SetFloat("moveY", moveInputVector.y);
+    }
+
+    private void Turn()
+    {
+        if (GetComponent<Rigidbody2D>().linearVelocity != Vector2.zero)
+        {
+            if (moveInputVector.x > 0) transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+            else if (moveInputVector.x < 0) transform.localScale = new Vector3(-Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+        }
+    }
+
+    public void Turn(Vector2 direction)
+    {
+        if (direction.x > 0) transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+        else if (direction.x < 0) transform.localScale = new Vector3(-Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
     }
 }
--- Assets/Scripts/Niveles/Lobby_Initiator.cs
+++ Assets/Scripts/Niveles/Lobby_Initiator.cs
@@ -1,11 +1,18 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
+using UnityEngine.UI;
 using System.Collections;
+using UnityEngine.InputSystem;
+
+
 
 public class Lobby_Initiator : MonoBehaviour
 {
     public InputActionAsset inputActions;
     public GameObject characterPrefab;
+    public Image fade;
+    public AnimationCurve fadeCurve;
+
+    private GameObject character;
 
     void Start()
     {
@@ -14,18 +21,35 @@ public class Lobby_Initiator : MonoBehaviour
 
     private IEnumerator InitializeLobby()
     {
-        // Completar Fade de Carga
-        yield return StartCoroutine(Game_Loader_Manager.Instance.CompleteLoadScene());
         
+        // Completar Fade de Carga
+        //yield return StartCoroutine(Game_Loader_Manager.Instance.CompleteLoadScene());
+        //yield return StartCoroutine(FadeBlanco());
+
         //Settear Personaje mientras se reproduce la cinemática
         bool spawned = false;
         StartCoroutine(SpawnCharacter(() => spawned = true));
-        //yield return StartCoroutine(CinematicaInicial());
+        /*StartCoroutine(EsperarYquitarFade(3f));
+        yield return StartCoroutine(CinematicaInicial());
         yield return new WaitUntil(() => spawned);
+        */
+
+        //Levantarse del sillon
+        yield return new WaitForSeconds(2f);
+        character.GetComponent<Animator>().SetTrigger("WakeUp");
+        
+        //Esperar a que se presione X
+        
+        
+        
+        
+        
+        
+        
         
         //Habilitar Input
-        inputActions.FindActionMap("Gameplay").Enable();
-
+        //inputActions.FindActionMap("Gameplay").Enable();
+        yield return null;
     }
 
     private IEnumerator CinematicaInicial()
@@ -42,9 +66,24 @@ public class Lobby_Initiator : MonoBehaviour
 
     private IEnumerator SpawnCharacter(System.Action onSpawned)
     {
-        GameObject character = Instantiate(characterPrefab);
+        character = Instantiate(characterPrefab,new Vector3(7.36f, 1.65f, 0f), Quaternion.identity);
+        character.GetComponent<Animator>().SetTrigger("Sit");
         onSpawned?.Invoke();
         yield return null;
+    }
+
+    private IEnumerator FadeBlanco()
+    {
+        fade.color = new Color(1f, 1f, 1f, 0f);
+        fade.gameObject.SetActive(true);
+        yield return DevTools.Animar(fade, 1f,3f, fadeCurve);
+        yield return null;
+    }
+
+    private IEnumerator EsperarYquitarFade(float duration)
+    {
+        yield return new WaitForSeconds(duration);
+        fade.gameObject.SetActive(false);
     }
 }
 
